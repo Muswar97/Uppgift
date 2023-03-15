a687850110db8a4a3038dfae24033a1e0c375f14
Muswar97 <122982911+Muswar97@users.noreply.github.com>
2023-03-15 18:33:41 +0100

    Update test_kontakt.py

--- test_kontakt.py
+++ test_kontakt.py
@@ -1,3 +1,4 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -6,22 +7,30 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-driver.get("https://www.elgiganten.se/")
-driver.maximize_window()
-try:
-    element = WebDriverWait(driver,100).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR,"#coiPage-1 > div.coi-banner__page-footer > button.coi-banner__accept"))
-    )
-    element.click()
-except:
-    driver.quit()
-link = driver.find_element(By.XPATH,"/html/body/elk-app/ng-component/elk-base-template/mat-sidenav-container/mat-sidenav-content/div[1]/elk-component-loader-wrapper/elk-header/header/elk-meta-controls/div[1]/a")
-link.click()
-kontakt = driver.find_element(By.CSS_SELECTOR,"#main > ng-component > div > elk-component-loader-wrapper > elk-cms-template-component-list > div > elk-cms-shell:nth-child(4) > elk-media-text > div > div.cms-media-text__text-wrapper > elk-wcag-button-link > a") 
-#driver.execute_script("arguments[0].scrollIntoView(true);", kontakt)
-driver.execute_script("scrollBy(0,1699)")
-time.sleep(1)
-kontakt.click()
-driver.execute_script("scrollBy(0,600)")
-time.sleep(10)
+
+class ElgigantenTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome(PATH)
+        self.driver.get("https://www.elgiganten.se/")
+        self.driver.maximize_window()
+    
+    def test_kontakta_oss_assert(self):
+        try:
+            element = WebDriverWait(self.driver,100).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR,"#coiPage-1 > div.coi-banner__page-footer > button.coi-banner__accept"))
+            )
+            element.click()
+        except:
+            self.driver.quit()
+        link = self.driver.find_element(By.XPATH,"/html/body/elk-app/ng-component/elk-base-template/mat-sidenav-container/mat-sidenav-content/div[1]/elk-component-loader-wrapper/elk-header/header/elk-meta-controls/div[1]/a")
+        link.click()
+        kontakt = self.driver.find_element(By.CSS_SELECTOR,"#main > ng-component > div > elk-component-loader-wrapper > elk-cms-template-component-list > div > elk-cms-shell:nth-child(4) > elk-media-text > div > div.cms-media-text__text-wrapper > elk-wcag-button-link > a") 
+        #driver.execute_script("arguments[0].scrollIntoView(true);", kontakt)
+        self.driver.execute_script("scrollBy(0,1299)")
+        time.sleep(1)
+        kontakt.click()
+        self.driver.execute_script("scrollBy(0,600)")
+        time.sleep(10)
+
+#assert self.driver.find_element(By.CSS_SELECTOR, ".flex-grid__item:nth-child(1) .align--center:nth-child(2)").text == "(+46) 0771 115 115\\\\nhello@elgiganten.se\\\\nVardagar: 08:30 - 20:00\\\\nHelg: Stängt"
