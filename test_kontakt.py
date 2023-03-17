b2b3d79b373be378b0985b58199e6461ef859d38
Muswar97 <122982911+Muswar97@users.noreply.github.com>
2023-03-17 15:43:12 +0100

    måste fixa ett fel!!

--- test_kontakt.py
+++ test_kontakt.py
@@ -13,7 +13,7 @@ class ElgigantenTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
         self.driver.get("https://www.elgiganten.se/")
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(1000)
         self.driver.maximize_window()
     
     def test_kontakta_oss_assert(self):
@@ -41,6 +41,7 @@ class ElgigantenTest(unittest.TestCase):
         )
         element.click()
         self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+        self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
         jobb = self.driver.find_element(By.XPATH,"/html/body/elk-app/ng-component/elk-base-template/mat-sidenav-container/mat-sidenav-content/elk-component-loader-wrapper/elk-footer/footer/div[2]/elk-navigation/div/mat-expansion-panel[4]/div/div/div/ul/li[3]/a")
         jobb.click()
         klicka = self.driver.find_element(By.CSS_SELECTOR,"#main > ng-component > elk-cms-shell > elk-sticky-header > div > elk-wcag-button-link > a")
@@ -55,16 +56,15 @@ class ElgigantenTest(unittest.TestCase):
         )
         element.click()
         search = self.driver.find_element(By.CSS_SELECTOR,"#query-header")
-        search.send_keys("Playstation 5")
+        search.send_keys("Elden Ring")
         search.send_keys(Keys.RETURN)
-        self.driver.implicitly_wait(10)
-        ps = self.driver.find_element(By.CSS_SELECTOR,"#products > elk-component-loader-wrapper > elk-product-and-content-listing-view > div.product-list__products.ng-star-inserted > elk-product-tile-ff-wc-wrapper:nth-child(1) > elk-product-tile > div > a")
-        self.driver.implicitly_wait(10)
-        ps.click()
-        btn = self.driver.find_element(By.CSS_SELECTOR,"#buy-box > elk-buy-box-delivery-tabs > elk-tab-group > div > div.tab-group__content > elk-buy-box-home-delivery-tab > elk-tab > div > elk-add-to-cart > button")
+        self.driver.find_element(By.CSS_SELECTOR,"#products > elk-component-loader-wrapper > elk-product-and-content-listing-view > div.product-list__products.ng-star-inserted > elk-product-tile-ff-wc-wrapper:nth-child(1) > elk-product-tile > div > a").click()
+        self.driver.find_element(By.CSS_SELECTOR,"#buy-box > elk-buy-box-delivery-tabs > elk-tab-group > div > div.tab-group__content > elk-buy-box-home-delivery-tab > elk-tab > div > elk-add-to-cart > button").click()
+        btn = WebDriverWait(self.driver,1000).until(
+           EC.presence_of_element_located((By.XPATH,"/html/body/div[7]/div[2]/div/mat-dialog-container/ng-component/div[4]/button[1]/span"))
+        )
         btn.click()
-        self.driver.find_element(By.CSS_SELECTOR,"#mat-dialog-10 > ng-component > div.buttons.sticky.ng-star-inserted > button.btn.btn--100.btn--3rd.btn--expanded").click()
-        
-        time.sleep(10)
-
- 
+        expected = "https://www.elgiganten.se/checkout"
+        time.sleep(100)
+        url = self.driver.current_url
+        self.assertEqual(url,expected)
