646dc3e7862d2603d710aa5ae42cf61faf195442
Muswar97 <122982911+Muswar97@users.noreply.github.com>
2023-03-20 18:03:53 +0100

    Klar med alla tester!
    Kan forsätta med att försöka ta bort sleep om jag har tid

--- test_kontakt.py
+++ test_kontakt.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 #PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -34,6 +35,7 @@ class ElgigantenTest(unittest.TestCase):
         self.assertTrue(info.is_displayed)
 
     def test_lediga_jobb_assert(self):
+        self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
         self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
         self.driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
         jobb = self.driver.find_element(By.XPATH,"/html/body/elk-app/ng-component/elk-base-template/mat-sidenav-container/mat-sidenav-content/elk-component-loader-wrapper/elk-footer/footer/div[2]/elk-navigation/div/mat-expansion-panel[4]/div/div/div/ul/li[3]/a")
@@ -96,6 +98,5 @@ class ElgigantenTest(unittest.TestCase):
         price2 = self.driver.find_element(By.XPATH,"/html/body/div[7]/div[2]/div/mat-dialog-container/elk-compare-overlay/div/div/div/elk-compare-scroller-group/div[1]/elk-compare-header/div/div/elk-compare-scroller/div/div/div/div[9]/elk-price/span/span").text
         p_expected2 = "3895.-"
         self.assertEqual(price2,p_expected2)
-        pop_up_window = self.driver.find_element(By.XPATH,"/html/body/div[7]/div[2]/div/mat-dialog-container/elk-compare-overlay/div/div/div")
+        time.sleep(2)
 
-        
